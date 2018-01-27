2c67ef0d20e2b6f7031cc59b76d602223023e0a7
joxar <mfpirlo@yahoo.co.jp>
2018-01-27 23:00:10 +0900

    first commit

---
+++ diffImage.py
@@ -0,0 +1,22 @@
+import cv2
+
+def diffAB(fileA, fileB):
+    imgA = cv2.imread(fileA)
+    imgB = cv2.imread(fileB)
+
+    grayA = cv2.cvtColor(imgA, cv2.COLOR_BGR2GRAY)
+    grayB = cv2.cvtColor(imgB, cv2.COLOR_BGR2GRAY)
+
+    hight, width = grayA.shape
+
+    result_window = np.zeros((hight, width), dtype=imgA.dtype)
+    for start_y in range(0, hight-100, 50):
+        for start_x in range(0, width-100, 50):
+            window = grayA[start_y:start_y+100, start_x:start_x+100]
+            match = cv2.matchTemplate(grayB, window, cv2.TM_CCOEFF_NORMED)
+            _, _, _, max_loc = cv2.minMaxLoc(match)
+            matched_window = grayB[max_loc[1]:max_loc[1]+100, max_loc[0]:max_loc[0]+100]
+            resutl = cv2.absdiff(window, matched_window)
+            result_window[start_y:start_y+100, start_x:start_x+100] = result
+
+    plt.imshow(result_window)
